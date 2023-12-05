85072285ba13cd87a79d6c08ffe55bb3339c999f
Loïc Ghijselings <l.ghijselings@student.helmo.be>
2023-12-05 23:53:20 +0100

    Refactor ProfileButton component and add LoginModal

--- components/ui/ProfileButton.jsx
+++ components/ui/ProfileButton.jsx
@@ -1,27 +1,24 @@
-import { useState } from "react";
-import Button from "@/components/ui/button";
+"use client";
+
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
+  DialogDescription,
   DialogTrigger,
 } from "@/components/ui/dialog";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { LoginModal } from "@/components/ui/LoginModal";
 
-const ProfileButton = () => {
-  const [login, setLogin] = useState(false);
-
+const ProfileButton = ({ currentUser }) => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
@@ -31,66 +28,28 @@ const ProfileButton = () => {
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-        {!login && (
+        {currentUser ? (
           <>
+            <DropdownMenuItem>Account</DropdownMenuItem>
+            <DropdownMenuItem>PLACEHOLDER</DropdownMenuItem>
+            <DropdownMenuItem>PLACEHOLDER</DropdownMenuItem>
+          </>
+        ) : (
+          <>
+            <DropdownMenuItem>Sign In</DropdownMenuItem>
             <Dialog>
               <DialogTrigger asChild>
-                <Button variant="outline">Share</Button>
+                <DropdownMenuItem
+                  onSelect={(e) => e.preventDefault()}
+                  className="cursor-pointer"
+                >
+                  Login
+                </DropdownMenuItem>
               </DialogTrigger>
-              <DialogContent className="sm:max-w-md">
-                <DialogHeader>
-                  <DialogTitle>Share link</DialogTitle>
-                  <DialogDescription>
-                    Anyone who has this link will be able to view this.
-                  </DialogDescription>
-                </DialogHeader>
-                <div className="flex items-center space-x-2">
-                  <div className="grid flex-1 gap-2">
-                    <Label htmlFor="link" className="sr-only">
-                      Link
-                    </Label>
-                    <Input
-                      id="link"
-                      defaultValue="https://ui.shadcn.com/docs/installation"
-                      readOnly
-                    />
-                  </div>
-                  <Button type="submit" size="sm" className="px-3">
-                    <span className="sr-only">Copy</span>
-                    <Copy className="h-4 w-4" />
-                  </Button>
-                </div>
-                <DialogFooter className="sm:justify-start">
-                  <DialogClose asChild>
-                    <Button type="button" variant="secondary">
-                      Close
-                    </Button>
-                  </DialogClose>
-                </DialogFooter>
+              <DialogContent>
+                <LoginModal />
               </DialogContent>
             </Dialog>
-            <DropdownMenuItem className="cursor-pointer">
-              REGISTER
-            </DropdownMenuItem>
-          </>
-        )}
-        {login && (
-          <>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem className="cursor-pointer">
-              PLACEHOLDER
-            </DropdownMenuItem>
-            <DropdownMenuItem className="cursor-pointer">
-              PLACEHOLDER
-            </DropdownMenuItem>
-            <DropdownMenuItem className="cursor-pointer">
-              PLACEHOLDER
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem className="cursor-pointer">
-              LOGOUT
-            </DropdownMenuItem>
           </>
         )}
       </DropdownMenuContent>
